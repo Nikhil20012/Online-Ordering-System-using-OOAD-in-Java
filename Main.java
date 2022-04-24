dbab22220e29ca1cb831b9712422310e310dbce4
Y Nikhil Bharadwaj <92667046+Nikhil20012@users.noreply.github.com>
2022-04-24 17:03:18 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import Boundary.LoginPage;
+import Boundary.ProductCatalogPage;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.start();
+        loginPage.login();
+
+        ProductCatalogPage productCatalogPage = new ProductCatalogPage();
+        productCatalogPage.start();
+        productCatalogPage.displayCatalogList();
+        productCatalogPage.selectCatalog();
+        
+    }
+}
